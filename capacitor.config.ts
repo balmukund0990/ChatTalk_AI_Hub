67ff80d758faabcc4f118edc4d0108078fcfd00c
Bal mukund kumar <balmukundkum123@gmail.com>
2026-03-13 06:31:50 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ChatTalk_AI_Hub',
+  webDir: 'www'
+};
+
+export default config;
